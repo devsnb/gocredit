a83942cf0bed439c0a2c483a061611fbbecec323
Thanmaichakka <thanmaichakka67@gmail.com>
2021-12-24 10:12:56 +0530

    made changes in  creditcardImpl layer

---
+++ gocredit-creditcard-service/src/main/java/com/gocredit/repository/ICreditCardRepository.java
@@ -0,0 +1,20 @@
+package com.gocredit.repository;
+
+import com.gocredit.exceptions.CardNotFoundException;
+import com.gocredit.model.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+@Re
+public interface ICreditCardRepository extends JpaRepository<CreditCard,Integer> {
+
+
+    @Query("from CreditCard c inner join c. where ")
+    List<CreditCard> findByUserAndNameOnCard(int userId, String nameOnCard) throws CardNotFoundException;
+    List<CreditCard>  findByUserAndType(int userId,String cardType) throws CardNotFoundException;
+    CreditCard findByCardNumber(String number) throws CardNotFoundException;
+    List<CreditCard> findByUserId(int userId) throws CardNotFoundException;
+
+}
---
+++ gocredit-creditcard-service/src/main/java/com/gocredit/service/CreditCardServiceImpl.java
@@ -0,0 +1,55 @@
+package com.gocredit.service;
+
+import com.gocredit.exceptions.CardNotFoundException;
+import com.gocredit.model.CreditCard;
+import com.gocredit.repository.ICreditCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CreditCardServiceImpl implements ICreditCardService{
+
+
+    ICreditCardRepository creditCardRepository;
+    @Autowired
+    public void setCreditCardRepository(ICreditCardRepository creditCardRepository) {
+        this.creditCardRepository = creditCardRepository;
+    }
+
+    @Override
+    public CreditCard addCard(CreditCard card) {
+        return creditCardRepository.save(card);
+    }
+
+    @Override
+    public void updateCard(CreditCard card) {
+       creditCardRepository.save(card);
+    }
+
+    @Override
+    public void deleteCard(int cardId) {
+        creditCardRepository.deleteById(cardId);
+    }
+
+    @Override
+    public List<CreditCard> getByUserAndNameOnCard(int userId, String nameOnCard) throws CardNotFoundException {
+        return ;
+    }
+
+    @Override
+    public List<CreditCard> getByUserAndType(int userId, String cardType) throws CardNotFoundException {
+        return null;
+    }
+
+    @Override
+    public CreditCard getByCardNumber(String number) throws CardNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<CreditCard> getByUserId(int userId) throws CardNotFoundException {
+        return null;
+    }
+}
